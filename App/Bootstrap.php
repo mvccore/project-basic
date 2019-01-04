9fb17249be26823d1dba8dd6e4c615e571599cdd
Tom Flídr <tomflidr@gmail.com>
2019-01-04 15:53:20 +0100

    code style

--- App/Bootstrap.php
+++ App/Bootstrap.php
@@ -6,20 +6,26 @@ class Bootstrap
 {
 	public static function Init () {
 
-		// patch core to use extended debug class
+		// Patch core to use extended debug class:
 		if (class_exists('\MvcCore\Ext\Debugs\Tracy')) {
 			\MvcCore\Ext\Debugs\Tracy::$Editor = 'NotepadPP';
 			\MvcCore\Application::GetInstance()->SetDebugClass('\MvcCore\Ext\Debugs\Tracy');
 		}
 		
-		// set up application routes with custom names:
+		// Set up application routes with custom names:
 		\MvcCore\Router::GetInstance([
-			'home'		=> [
-				'match'		=> "#^/$#",
-				'reverse'	=> '/',
-				'controller'=> 'Index',
-				'action'	=> 'Home',
+			'home'			=> [
+				'match'			=> "#^/$#",
+				'reverse'		=> '/',
+				'controller'	=> 'Index',
+				'action'		=> 'Home',
 			],
+			/*'my_name'	=> [
+				'pattern'		=> "/my-path[/<my_param>]',
+				'controller'	=> 'MyController',
+				'action'		=> 'Index',
+				'my_param'		=> NULL,
+			],*/
 		]);
 
 	}
